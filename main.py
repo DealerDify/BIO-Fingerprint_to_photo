bcb062adadba9d7df3363e4f806d10c77a91e589
Dify <lukas.havlicek@mensa.cz>
2021-11-02 23:59:55 +0100

    well, mapping skin to fingerprint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from PIL import Image, ImageFilter, ImageDraw
+from PIL import Image, ImageFilter, ImageDraw, ImageEnhance, ImageOps
 import numpy as np
 from scipy import ndimage
 import scipy.misc
@@ -9,6 +9,7 @@ def get_mask(src_img):
     # laplace
     A = np.full((9, 9), -1)
     A[4, 4] = 80
+
 
     # get edges
     out = ndimage.convolve(img, A)
@@ -23,11 +24,11 @@ def get_mask(src_img):
     reds = (n_mask[:, :, 0] == 255) & (n_mask[:, :, 1] == 0) & (n_mask[:, :, 2] == 0)
     return Image.fromarray((reds*255).astype(np.uint8))
 
-def change_color(src_img):
+def change_color(src_img, rgb_in, rgb_out):
     data = np.array(src_img.convert("RGB"))
 
-    r1, g1, b1 = 0, 0, 0  # Original value
-    r2, g2, b2 = 198, 140, 83  # Value that we want to replace it with
+    r1, g1, b1 = rgb_in  # Original value
+    r2, g2, b2 = rgb_out  # Value that we want to replace it with
 
     red, green, blue = data[:, :, 0], data[:, :, 1], data[:, :, 2]
     mask = (red == r1) & (green == g1) & (blue == b1)
@@ -43,14 +44,34 @@ def white_to_transparent(src_img):
     return Image.fromarray(x)
 
 if __name__ == '__main__':
-    pic = Image.open("C:\\Users\\Lukyn\\Desktop\\VUT_FIT\\bio\\Synth\\Synth\\sfinge\\SG_1_1.png")
-    #pic = pic.convert("L")
-
-    pic.show()
-
+    pic = Image.open("SG_3_1.png")
+    skin = Image.open("skin_texture_2.png")
+    skin = skin.resize(pic.size)
+    skin_darker = skin.copy()
+    factor = 0.95  # used between papillary lines
+    ultra_dark_factor = 0.5  # used on edged
+    skin_darker = ImageEnhance.Brightness(skin_darker).enhance(factor)
+    skin_ultra_darker = ImageEnhance.Brightness(skin_darker).enhance(ultra_dark_factor)
+    white_bg = Image.new("RGB", pic.size, color="white")
+    papillar = white_bg.copy()
+    darker_edges = white_bg.copy()
 
     mask = get_mask(pic)
-    #mask.show()
-    finger = change_color(mask)
-    pic = white_to_transparent(pic)
-    Image.blend(pic.convert('RGBA'), finger.convert('RGBA'), alpha=0.9).show()
+    blur_factor = 50
+    blurred_mask = mask.filter(ImageFilter.GaussianBlur(blur_factor))
+
+    invert_blurred_mask = ImageOps.invert(blurred_mask)
+    inverted_mask = ImageOps.invert(mask)
+    darker_edges.paste(invert_blurred_mask, (0, 0), inverted_mask)
+
+    orig_pic = white_to_transparent(pic)  # basically a mask from fingerprint
+
+    papillar.paste(skin, (0, 0), orig_pic.convert('RGBA'))  # maps skin to papillary lines
+    skin_darker.paste(pic, (0, 0), mask)  # cuts finger shape into skin
+
+    # combines papillary lines and finger "background"
+    orig_img = Image.blend(papillar.convert('RGBA'), skin_darker.convert('RGBA'), alpha=0.8)
+
+    # makes edges darker
+    final = Image.composite(orig_img, skin_ultra_darker, darker_edges.convert('L'))
+    final.show()
